9b5a0651388d950780cd22b262ac756e3c6fe090
Abhilash332 <srirangamabhilash93@gmail.com>
2026-01-03 15:22:31 +0530

    updated home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import Navbar from "./NavBar/Nav"; 
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Components/Nav"; 
+import Home from "./Components/Home"; // We will create this next
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Navbar /> 
-      <h1>Main Content Goes Here</h1>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-slate-50">
+        <Navbar /> 
+        
+        {/* The Routes container decides which component to show */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<div className="p-10 text-2xl">Search Alumni Page</div>} />
+          <Route path="/events" element={<div className="p-10 text-2xl">Events Page</div>} />
+          <Route path="/dashboard" element={<div className="p-10 text-2xl">Dashboard Page</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
