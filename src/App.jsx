443f6741ffa7bd03433011a5b89693ea0fc46f56
Abhilash332 <srirangamabhilash93@gmail.com>
2026-01-04 11:25:25 +0530

    All issues got cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./Components/Nav"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Nav";
 import Home from "./Components/Home";
-import './App.css';
-import Signup from './Components/Signup';
-import Login from './Components/Login';
-import Events from './Components/Events';
-import ScrollToTop from "./components/ScrollToTop";
+import "./App.css";
+import Signup from "./Components/SignUp";
+import Login from "./Components/Login";
+import Events from "./Components/EVENTS/Events";
+import RegisterEventPage from "./Components/EVENTS/RegisterEventPage";
+import ViewAgendaPage from "./Components/EVENTS/ViewAgendaPage";
+import SubmitEventProposalPage from "./Components/EVENTS/SubmitEventProposalPage";
+import HostGuidelinesPage from "./Components/EVENTS/HostGuidelinesPage";
+import ScrollToTop from "./Components/ScrollToTop";
 import JobReferrals from './Components/JobReferrals';
 
 function App() {
@@ -13,15 +17,30 @@ function App() {
     <Router>
       <div className="min-h-screen bg-slate-50">
         <Navbar />
-        <ScrollToTop /> 
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/search" element={<div className="p-10 text-2xl">Search Alumni Page</div>} />
-          <Route path="/events" element={<Events/>} />
-          <Route path="/dashboard" element={<div className="p-10 text-2xl">Dashboard Page</div>} />
-          <Route path="/jobs" element={<JobReferrals/>}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/events">
+            <Route index element={<Events />} />
+            <Route path="register" element={<RegisterEventPage />} />
+            <Route path="agenda" element={<ViewAgendaPage />} />
+            <Route
+              path="submit-proposal"
+              element={<SubmitEventProposalPage />}
+            />
+            <Route path="guidelines" element={<HostGuidelinesPage />} />
+          </Route>
+          <Route
+            path="/search"
+            element={<div className="p-10 text-2xl">Search Alumni Page</div>}
+          />
+          <Route
+            path="/dashboard"
+            element={<div className="p-10 text-2xl">Dashboard Page</div>}
+          />
+          <Route path="/jobs" element={<JobReferrals />} />
         </Routes>
       </div>
     </Router>
